2f8eb4013cd057909cd4d24b422f8f408d262350
John Griebel <jkgriebel93@gmail.com>
2025-06-16 09:51:54 -0400

    Add basic REST endpoints for core app
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- core/views.py
+++ core/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import (GriddyBaseModel,
+                         Venue,
+                         Franchise,
+                         League,
+                         Conference,
+                         Division,
+                         Team)
+from core.serializers import (VenueSerializer,
+                              FranchiseSerializer,
+                              LeagueSerializer,
+                              ConferenceSerializer,
+                              DivisionSerializer,
+                              TeamSerializer)
+
+
+class VenueViewSet(viewsets.ModelViewSet):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+
+class FranchiseViewSet(viewsets.ModelViewSet):
+    queryset = Franchise.objects.all()
+    serializer_class = FranchiseSerializer
+
+
+class LeagueViewSet(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+
+class ConferenceViewSet(viewsets.ModelViewSet):
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+
+
+class DivisionViewSet(viewsets.ModelViewSet):
+    queryset = Division.objects.all()
+    serializer_class = DivisionSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
